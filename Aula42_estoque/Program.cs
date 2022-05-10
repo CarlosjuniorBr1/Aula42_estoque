2fab54f91f3308eb6c00dc6620cc9520cb3be22a
CarlosJuniorBr <cjr1415@outlook.com>
2022-05-10 08:44:52 -0300

    Add project files.

---
+++ Aula42_estoque/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Aula42_estoque;
+
+namespace Course
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do Produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+
+        }
+    }
+}
